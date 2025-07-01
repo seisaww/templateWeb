98e8590acbc9e64157ec2049557702f447ede30e
emmamau <emmanuel.maurice@gmail.com>
2025-07-01 15:50:26 +0200

    Update environment.prod.ts

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendLoginClient: 'https://api-sha-1b4a12d.onrender.com/api/utilisateur/login'
+  backendLoginClient: 'https://api-n7fb.onrender.com/m/api/utilisateur/login'
 };
