65e0ec79d97d2c24e47efa42689c98ed5b288e5e
Imane <laazibiimane101@gmail.com>
2025-10-30 15:50:48 +0100

    credentials

--- api/config.js
+++ api/config.js
@@ -1,11 +1,11 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d40798bipnbc73cf4njg-a.oregon-postgres.render.com",
+    "host" :"dpg-d41n3fripnbc73fhcem0-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "cnam_qnjc_user",
-    "password" : "FOYjmLxOmqwdnfGPhMO4R8eZpErPzVXQ",
-    "bdname" :"cnam_qnjc" 
+    "user" : "pollution_gcrl_user",
+    "password" : "gORmSbz8W4Cyl4CNuUmxIrXnjw5Q5jbd",
+    "bdname" :"pollution_gcrl" 
     }
 }
 
