19b99fbcee09bb35914bf1d2ec1fdf9c903cf73b
Imane <laazibiimane101@gmail.com>
2025-11-29 11:04:13 +0100

    Nouvelle configuration base de donnée

--- api/config.js
+++ api/config.js
@@ -1,11 +1,13 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d41n3fripnbc73fhcem0-a.oregon-postgres.render.com",
+    "host" :"dpg-d4kq9eggjchc73abiq50-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "pollution_gcrl_user",
-    "password" : "gORmSbz8W4Cyl4CNuUmxIrXnjw5Q5jbd",
-    "bdname" :"pollution_gcrl" 
+    "user" : "pollution_7sn8_user",
+    "password" : "BI2ik6z510M4rYxl7IOpm922QNwrCffT",
+    "bdname" :"pollution_7sn8" 
     }
 }
+
+
 
